314eb5d23229ab2977705bdb1377a739f9c77573
sophie-nguyen-0 <113055711+sophie-nguyen-0@users.noreply.github.com>
2022-11-27 16:02:51 +0000

    cs

--- main.cs
+++ main.cs
@@ -1,7 +1,34 @@
+// Created by: sophie
+// Created on: oct 2020
+//
+// This program calculates area and perimeter of a rectangle
+
 using System;
 
-class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
-  }
+class Program
+{
+    public static void Main(string[] args)
+    {
+        string day = null;
+        int age;
+
+        System.Random random = new System.Random();
+
+        Console.Write("Enter a day of the week: ");
+        day = Convert.ToString(Console.ReadLine());
+
+        Console.Write("Enter a day of the week: ");
+        age = Convert.ToInt32(Console.ReadLine());
+
+        if ((day == "Thursday" || day == "Tuesday") || (age >= 12 && age <= 21))
+        {
+            Console.WriteLine("you can get a discount!");
+        }
+        else
+        {
+            Console.WriteLine("sorry, you can't get a discount");
+        }
+
+        Console.WriteLine("\nDone.");
+    }
 }
